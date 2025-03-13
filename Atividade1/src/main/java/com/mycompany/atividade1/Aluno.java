18d692b2a0f73681c0e16fe96959be8a22bd7fdf
Danielle Gonçalves Ferreira <160173090+daniferreira05@users.noreply.github.com>
2025-03-13 10:53:40 -0300

    Add files via upload

---
+++ Atividade1/src/main/java/com/mycompany/atividade1/Aluno.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.atividade1;
+
+
+public class Aluno {
+    String matricula;//estipular uma ano "fixo" + 4 digitos aleatórios(random)
+    String nome;//Sempre registado com letra maiúscula.//
+    String curso;
+    char Turma;
+    int periodo;
+    double nota1B;
+    double nota2B;
+    double notaFinal;
+    public static int  qtdAlunos;//para acada aluno criado nas intnacias o programa deve 'conta-los' e mostrar o numero total de alunos\\
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
